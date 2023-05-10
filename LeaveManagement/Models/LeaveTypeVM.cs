7e634f40fb10f626a932af3d84702a993206074e
pranj <pranjaljoshi7513@gmail.com>
2023-05-11 00:27:12 +0530

    Modified the crud screens and added datatables

---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Default no of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage ="Please enter a vaild Number")]
+        public int DefaultDays { get; set; }
+    }
+}
